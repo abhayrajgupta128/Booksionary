42f52863eb21927e78c6fe3b7530f66663b49e60
Abhay Raj Gupta <abhayrajgupta128@gmail.com>
2024-04-30 23:58:26 +0530

    27

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     methods: '*',
-    origin: '*',
+    origin: 'https://booksionary-client.vercel.app/',
   })
 );
 
