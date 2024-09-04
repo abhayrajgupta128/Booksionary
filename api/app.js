0890d49914fabe3c77e57f9f0eced565cdb0ea8f
Abhay Raj Gupta <111484446+abhayrajgupta128@users.noreply.github.com>
2024-09-04 20:07:48 +0530

    cors fix

--- api/app.js
+++ api/app.js
@@ -17,7 +17,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://booksionary.vercel.app",
   })
 );
 
